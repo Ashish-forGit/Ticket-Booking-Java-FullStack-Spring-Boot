2b4fa82be37d4dbf347f2b5fe94efc267caecebf
Ashish Kumar <ashish93343@gmail.com>
2025-06-09 20:02:34 +0530

    learning objects serialization through object mapper

--- app/src/main/java/org/example/services/UserBookingService.java
+++ app/src/main/java/org/example/services/UserBookingService.java
@@ -1,12 +1,22 @@
 package org.example.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.entities.User;
+
+import java.io.File;
 
 public class UserBookingService {
     private User user;
 
+    private ObjectMapper  objectMapper = new ObjectMapper();
+
+    private static final String USERS_PATH = "../localDb/users.jason";
+
      public UserBookingService(User user1) {
+
          this.user = user1;
+         File user = new File(USERS_PATH);
+
      }
 
 }
