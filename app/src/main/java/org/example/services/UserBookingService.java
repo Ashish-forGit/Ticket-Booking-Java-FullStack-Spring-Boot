78f8421fef597e07c145dee6092d57493ce06b27
Ashish Kumar <ashish93343@gmail.com>
2025-06-11 21:42:41 +0530

    added data desearialization

--- app/src/main/java/org/example/services/UserBookingService.java
+++ app/src/main/java/org/example/services/UserBookingService.java
@@ -1,21 +1,26 @@
 package org.example.services;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.entities.User;
 
 import java.io.File;
+import java.util.List;
 
 public class UserBookingService {
     private User user;
 
-//    private ObjectMapper  objectMapper = new ObjectMapper();
+    private ObjectMapper  objectMapper = new ObjectMapper();
+
+    private List<User> userList;
 
     private static final String USERS_PATH = "../localDb/users.jason";
 
      public UserBookingService(User user1) {
 
          this.user = user1;
-         File user = new File(USERS_PATH);
+         File users = new File(USERS_PATH);
+         userList = objectMapper.readValue(users, TypeReference< List<User>>() {});
 
      }
 
